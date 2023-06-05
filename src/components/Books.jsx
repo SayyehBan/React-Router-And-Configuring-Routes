413126df199b216d7a7656cd0b7151ea76142813
SayyehBan <sdvp1991david@gmail.com>
2023-06-05 14:01:17 +0330

    استفاده از useLocation و useSearchParams

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,30 +1,60 @@
-import React from 'react'
-import { getBooks } from '../data/data'
-import {  NavLink, Outlet } from 'react-router-dom';
+import React from "react";
+import { getBooks } from "../data/data";
+import {
+  NavLink,
+  Outlet,
+  useLocation,
+  useSearchParams,
+} from "react-router-dom";
 
 const Books = () => {
-    const books = getBooks();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  const books = getBooks();
   return (
-       <div style={{ display:'flex' }}>
-          <nav style={{ borderLeft:'solid 1px', padding: "1rem" }} >
-              <input type="text" placeholder='جستجو کتاب' />
+    <div style={{ display: "flex" }}>
+      <nav style={{ borderLeft: "solid 1px", padding: "1rem" }}>
+        <input
+          type="text"
+          value={searchParams.get("filter") || ""}
+          onChange={(e) => {
+            let filter = e.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+          placeholder="جستجو کتاب"
+        />
 
-              <br/>
-              {books.map((book) => (
-                <NavLink style={({ isActive }) => {
-                  return {
-                    display: 'block',
-                    margin: '1rem 0',
-                    color:isActive?"red":"black"
-                  }
-                  }} to={`/books/${book.number}`} key={book.number}>
-                      {book.name}
-                      </NavLink>
-              ))}
+        <br />
+        {books
+          .filter((book) => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = book.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((book) => (
+            <NavLink
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : "black",
+                };
+              }}
+              to={`/books/${book.number}${location.search}`}
+              key={book.number}
+            >
+              {book.name}
+            </NavLink>
+          ))}
       </nav>
-      <Outlet/>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
